0ec32da6bcac9d90eab826f9d23cd39c84da32a2
quangnm2293 <quangmn2293@gmail.com>
2021-09-20 10:46:01 +0700

    fix issue range error

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import { DataProvider } from '../store/GlobalState';
 import '../styles/globals.css';
 import Notify from '../components/tailwind/Notify';
-// import { ErrorBoundary } from '../components/tailwind/ErrorBoundary';
+import { ErrorBoundary } from '../components/tailwind/ErrorBoundary';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		// <ErrorBoundary>
-		<DataProvider>
-			<Notify />
+		<ErrorBoundary>
+			<DataProvider>
+				<Notify />
 
-			<Component {...pageProps} />
-		</DataProvider>
-		// </ErrorBoundary>
+				<Component {...pageProps} />
+			</DataProvider>
+		</ErrorBoundary>
 	);
 }
 
